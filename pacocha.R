9db3a10caf870c72bf9c20a0ac4a151f93ee639d
JPacoch <pacochp@gmail.com>
2019-05-18 23:33:29 +0200

    kwadrat, czworobok i prostokat v1

--- pacocha.R
+++ pacocha.R
@@ -106,3 +106,95 @@ hwdp_plot.sqr <- function(x1, y1, x2, y2, x3, y3, x4, y4){
 
 hwdp_plot.sqr(0,0,5,0,5,5,0,5) 
 
+# Czworobok
+
+# Obliczanie obwodu czworoboku na podstawie odległości wierzchołków.
+
+hwdp_par.pol <- function(x1, y1, x2, y2, x3, y3, x4, y4){
+  a <- length.line(x1, y1, x2, y2)
+  b <- length.line(x2, y2, x3, y3)
+  c <- length.line(x3, y3, x4, y4)
+  d <- length.line(x4, y4, x1, y1)
+    a + b + c + d
+} 
+
+# Rysowanie czworoboku na podstawie współrzędnych jego wierzchołków.
+
+hwdp_plot.pol <- function(x1, y1, x2, y2, x3, y3, x4, y4){
+  x <- c(x1, x2, x3, x4)
+  y <- c(y1, y2, y3, y4)
+  plot(x, y)
+  lines(c(x[1], x[2]), c(y[1], y[2]))
+  lines(c(x[2], x[3]), c(y[2], y[3]))
+  lines(c(x[3], x[4]), c(y[3], y[4]))
+  lines(c(x[4], x[1]), c(y[4], y[1]))
+}
+
+
+# Prostokąt 
+
+# Czy figura jest prostokątem?
+
+hwdp_can.rec <- function(x1, y1, x2, y2, x3, y3, x4, y4){
+  a <- length.line(x1, y1, x2, y2)
+  b <- length.line(x2, y2, x3, y3)
+  c <- length.line(x3, y3, x4, y4)
+  d <- length.line(x4, y4, x1, y1)
+  if (a == c){
+    TRUE
+  } else {
+    FALSE
+  }
+  if (b == d){
+    TRUE
+  } else {
+    FALSE
+  }
+}
+
+hwdp_area.rec(0,0,4,0,4,5,0,5)
+
+# Obwód prostokąta na podstawie sumy odległości wierzchołków. 
+
+hwdp_par.rec <- function(x1, y1, x2, y2, x3, y3, x4, y4){
+  a <- length.line(x1, y1, x2, y2)
+  b <- length.line(x2, y2, x3, y3)
+  if (hwdp_can.rec(x1, y1, x2, y2, x3, y3, x4, y4) == FALSE){
+    stop("The geometric figure is not a rectangle") 
+  } else {
+    2 * a + 2 * b
+  }
+} 
+
+# Pole prostokąta na podstawie iloczynu jego boków.
+
+hwdp_area.rec <- function(x1, y1, x2, y2, x3, y3, x4, y4){
+  a <- length.line(x1, y1, x2, y2)
+  b <- length.line(x2, y2, x3, y3)
+  if (hwdp_can.rec(x1, y1, x2, y2, x3, y3, x4, y4) == FALSE){
+    stop("The geometric figure is not a rectangle") 
+  } else {
+    a * b
+  }
+} 
+
+hwdp_dia.rec <- function(x1, y1, x2, y2, x3, y3, x4, y4){
+  a <- length.line(x1, y1, x2, y2)
+  b <- length.line(x2, y2, x3, y3)
+  if (hwdp_can.rec(x1, y1, x2, y2, x3, y3, x4, y4) == FALSE){
+    stop("The geometric figure is not a rectangle") 
+  } else {
+    sqrt(a ^ 2 * b ^ 2)
+  }
+} 
+
+
+hwdp_plot.rec <- function(x1, y1, x2, y2, x3, y3, x4, y4){
+  x <- c(x1, x2, x3, x4)
+  y <- c(y1, y2, y3, y4)
+  plot(x, y)
+  lines(c(x[1], x[2]), c(y[1], y[2]))
+  lines(c(x[2], x[3]), c(y[2], y[3]))
+  lines(c(x[3], x[4]), c(y[3], y[4]))
+  lines(c(x[4], x[1]), c(y[4], y[1]))
+}
