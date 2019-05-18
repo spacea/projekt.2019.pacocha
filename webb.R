c876e475f5daf69396a1faab8f11bec914868d3a
partykp <48154165+partykp@users.noreply.github.com>
2019-05-18 15:29:42 +0200

    Add files via upload
    bardziej przejrzyste, wciąż nie działa

--- webb.R
+++ webb.R
@@ -8,52 +8,38 @@ WB <- c(NA)
 webb <- function(PN,SM) {
 
   while (i <= length(SM)) {
-    if (PN > 0 && SM > 0) { #B i C
-      
+    
+    if (PN[i] > 0 && SM[i] > 0) { #B i C
       if (PN[i]>SM[i]){
         WB[i] <-  "B"
-      }
-      
-      if (PN[i]<SM[i]){
+    } else if (PN[i]<SM[i]){
         WB[i] <-  "C"
       }
-    }
-    
-    else if (PN > 0 && SM < 0) { #A i H
       
+  } else if (PN[i] > 0 && SM[i] < 0) { #A i H
       if (PN[i]>SM[i]){
         WB[i] <-  "A"
-      }
-      
-      if (PN[i]<SM[i]){
+    } else if (PN[i]<SM[i]){
         WB[i] <-  "H"
       }
-    }
     
-    else if (PN < 0 && SM > 0) { #E i D
-      
+  } else if (PN[i] < 0 && SM[i] > 0) { #E i D
       if (PN[i]>SM[i]){
         WB[i] <-  "E"
-      }
-      
-      if (PN[i]<SM[i]){
+    } else if (PN[i]<SM[i]){
         WB[i] <-  "D"
       }
-    }
     
-    else if (PN < 0 && SM < 0) { #F i G
-      
+  } else if (PN[i] < 0 && SM[i] < 0) { #F i G
       if (PN[i]>SM[i]){
         WB[i] <-  "G"
-      }
-      
-      if (PN[i]<SM[i]){
+    } else if (PN[i]<SM[i]){
         WB[i] <-  "F"
       }
-    }
-    else {
-      print("równe")
-    }
+  
+   } else {
+        print("=")
+      }
     
    i <-  i+1
   }#while
